3b061e3faaea5b2cedf0d73f915d7f0e81c16654
richmtx <martinezhernandezricardo06@gmail.com>
2026-02-24 14:40:50 -0600

    Commit

--- src/app/pages/egresados2/egresados2.component.ts
+++ src/app/pages/egresados2/egresados2.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-egresados2',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './egresados2.component.html',
   styleUrl: './egresados2.component.css'
 })
